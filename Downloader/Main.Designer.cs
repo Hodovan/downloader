ea4525785aedcaa52e025d88ad743a988a90a3ca
KisHodi <KisHodi@users.noreply.github.com>
2020-05-20 23:33:56 +0200

    add sync and async download buttons

---
+++ Downloader/Main.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Downloader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownloadSync = new System.Windows.Forms.Button();
+            this.btnDownloadAsync = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnDownloadSync
+            // 
+            this.btnDownloadSync.Location = new System.Drawing.Point(13, 13);
+            this.btnDownloadSync.Name = "btnDownloadSync";
+            this.btnDownloadSync.Size = new System.Drawing.Size(160, 30);
+            this.btnDownloadSync.TabIndex = 0;
+            this.btnDownloadSync.Text = "Sync. Download";
+            this.btnDownloadSync.UseVisualStyleBackColor = true;
+            this.btnDownloadSync.Click += new System.EventHandler(this.btnDownloadSync_Click);
+            // 
+            // btnDownloadAsync
+            // 
+            this.btnDownloadAsync.Location = new System.Drawing.Point(13, 49);
+            this.btnDownloadAsync.Name = "btnDownloadAsync";
+            this.btnDownloadAsync.Size = new System.Drawing.Size(160, 30);
+            this.btnDownloadAsync.TabIndex = 1;
+            this.btnDownloadAsync.Text = "Async. Download";
+            this.btnDownloadAsync.UseVisualStyleBackColor = true;
+            this.btnDownloadAsync.Click += new System.EventHandler(this.btnDownloadAsync_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(412, 13);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(160, 30);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "Open File in Win. Explorer";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnDownloadAsync);
+            this.Controls.Add(this.btnDownloadSync);
+            this.Name = "Main";
+            this.Text = "Downloader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownloadSync;
+        private System.Windows.Forms.Button btnDownloadAsync;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Downloader/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+
+
+namespace Downloader
+{
+    public partial class Main : Form
+    {
+
+
+        WebClient client = new WebClient();
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownloadSync_Click(object sender, EventArgs e)
+        {
+            // DOWNLOAD HAS THE EFFECT OF FREEZING THE INTERFACE WHILE DOWNLOAD OCCURS
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                client.DownloadFile("https://github.com/git-for-windows/git/releases/download/v2.26.2.windows.1/Git-2.26.2-64-bit.exe", saveFileDialog.FileName);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe","/select," + saveFileDialog.FileName);
+        }
+
+        private void btnDownloadAsync_Click(object sender, EventArgs e)
+        {
+            // THE INTERFACE DOES NOT FREEZE DURING THE DOWNLOAD
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                client.DownloadFileAsync(new Uri("https://github.com/git-for-windows/git/releases/download/v2.26.2.windows.1/Git-2.26.2-64-bit.exe"), saveFileDialog.FileName);
+            }
+        }
+    }
+}
